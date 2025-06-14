f22fc6a2b11904e6418ac1eff5825052afb04f31
dancannotcode <dancannotcode@gmail.com>
2025-06-13 17:39:59 -0700

    Updates

---
+++ assets/about-BOdDdeDS.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,r,H as o}from"./Header-BTKpjibN.js";/* empty css              */e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
--- assets/contact-4CJP1Bvs.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,r,H as o}from"./Header-DE1p8kfe.js";/* empty css              */e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
