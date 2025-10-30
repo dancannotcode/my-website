30a42f91e2d7af6bcde20927c4533f84e5200bde
dancannotcode <dancannotcode@gmail.com>
2025-10-30 10:24:20 -0700

    Updates

--- assets/about-BsWjBqWU.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,r,H as o}from"./Header-TqcC_r48.js";/* empty css              */e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
